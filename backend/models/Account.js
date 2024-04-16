078751d239ee64bb681871593b9cdba086d05fd7
npanttonen <113436924+npanttonen@users.noreply.github.com>
2024-04-16 13:40:07 +0300

    Update Account.js

--- backend/models/Account.js
+++ backend/models/Account.js
@@ -8,6 +8,10 @@ const AccountSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
     }
 });
 
