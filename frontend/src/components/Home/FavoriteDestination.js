c906aecd1c7a3863d64ce2e8a7653dcc8e3badd8
npanttonen <113436924+npanttonen@users.noreply.github.com>
2024-04-23 10:17:49 +0300

    Update FavoriteDestination.js

--- frontend/src/components/Home/FavoriteDestination.js
+++ frontend/src/components/Home/FavoriteDestination.js
@@ -1,62 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HomeCSS from './home.module.css';
-// import trainStationImage from '../../assets/trainStation3.jpg';
-import HelsinkiImage from '../../assets/Helsinki.jpg';
-import TurkuImage from '../../assets/Turku.jpg';
-import TampereImage from '../../assets/Tampere.jpg';
 import { MdLocationPin } from "react-icons/md";
+import axios from 'axios';
 
 const FavoriteDestination = () => {
+  const [favoriteDestinations, setFavoriteDestinations] = useState([]);
 
+  useEffect(() => {
+    const fetchFavoriteDestinations = async () => {
+      try {
+        const token = localStorage.getItem('accessToken'); 
 
-  const FavoriteDestinationCity = [
-    {
-      id: 1,
-      Departure: 'Helsinki',
-      Destination: 'Tampere',
-     image: TampereImage
+        // Set up the headers with the authentication token
+        const headers = {
+          'authorization': `Bearer ${token}`
+        };
 
-    },
-    {
-        id: 2,
-        Departure: 'Tampere',
-        Destination: 'Helsinki',
-       image: HelsinkiImage
-      },
-    {
-        id: 3,
-        Departure: 'Helsinki',
-        Destination: 'Turku',
-       image: TurkuImage,
-      },
+        // Make a GET request to fetch favorite destinations from the backend
+        const response = await axios.get('http://localhost:3080/getfavorite', { headers });
+        setFavoriteDestinations(response.data.favoriteTrips);
+      } catch (error) {
+        console.error('Error fetching favorite destinations:', error);
+      }
+    };
 
-
-  ];
+    fetchFavoriteDestinations();
+  }, []);
 
   return (
     <div className="educational-background container mt-5">
       <h2 className={`mb-4 ${HomeCSS.destination}`}>Favorite Destinations </h2>
       <div className="row justify-content-center">
-
-        {/* iterate through the objects, they are treated as an array of objects */}
-        {FavoriteDestinationCity.map((FavoriteDestinationCityItem) => (
-          <div className="col-md-6 col-lg-4 mb-4" key={FavoriteDestinationCityItem.id} style={{ height: '300px',  width: '300px' }}>
+        {/* iterate through the favorite destinations fetched from the backend */}
+        {favoriteDestinations.map((favoriteDestinationItem) => (
+          <div className="col-md-6 col-lg-4 mb-4" key={favoriteDestinationItem._id} style={{ height: '300px',  width: '300px' }}>
             <div className="card h-100 shadow">
-            <img src={FavoriteDestinationCityItem.image} className={`card-img-top ${HomeCSS.cardImage}`} alt="City" />
+              <img src={favoriteDestinationItem.image} className={`card-img-top ${HomeCSS.cardImage}`} alt="City" />
               <div className="card-header text-muted">
-                <h5 className="card-title mb-0">{FavoriteDestinationCityItem.Departure}</h5>
+                <h5 className="card-title mb-0">{favoriteDestinationItem.departure}</h5>
               </div>
               <div className="card-body">
                 <h6 className={`card-subtitle mb-2 bold ${HomeCSS.card}`}>
-                <MdLocationPin />{FavoriteDestinationCityItem.Destination}
+                  <MdLocationPin />{favoriteDestinationItem.destination}
                 </h6>
                 <div className="d-flex justify-content-center">
-                {/* Need to create a button to autofill search options */}
-                {/* <a href="" target="_blank" rel="noopener noreferrer" className={`btn btn-dark ${ProjectCSS.socialButton}`}>
-                </a> */}
+                  {/* Need to create a button to autofill search options */}
+                  {/* <a href="" target="_blank" rel="noopener noreferrer" className={`btn btn-dark ${ProjectCSS.socialButton}`}>
+                  </a> */}
+                </div>
               </div>
-              </div>
-
             </div>
           </div>
         ))}
@@ -64,4 +56,5 @@ const FavoriteDestination = () => {
     </div>
   );
 };
+
 export default FavoriteDestination;
